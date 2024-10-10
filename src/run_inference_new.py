5e30b925c49b887cbdafb7b7555bea6c530b7848
giorgiobini <giorgio.bini13@gmail.com>
2024-10-10 08:45:05 +0000

    updts

--- src/run_inference_new.py
+++ src/run_inference_new.py
@@ -26,7 +26,7 @@ from config import *
 
 def get_args_parser():
     parser = argparse.ArgumentParser('Set model args', add_help=False)
-    parser.add_argument('--model_name', default='all_modelarch2_wrongSN_easypretrain11_paristfinetuningSPLASHval0039parisHQSPLASHval0054_okSN_splashPARISFINETUNINGtrainhqRICSEQval0060_NOWENHNokSN_splashPARISFINETUNINGtrainhqRICSEQval0083',
+    parser.add_argument('--model_name', default='arch2_PARISfinetuned_PARIStest0023_PARISfinetunedFPweight_PARIStest0086',
                         help='Name of the model folder')
     parser.add_argument('--pairs_path', default='',
                         help='Path to the folder where there is pairs.csv')
@@ -37,6 +37,7 @@ def get_args_parser():
 def main(args, device):
 
     df = pd.read_csv(os.path.join(pairs_path, 'pairs.csv'))
+    df = df.filter(['id_pair','embedding1name','embedding2name','start_window1','end_window1','start_window2','end_window2'], axis = 1)
     print(df.shape[0], 'sequences to download')
     
     model = build_model(args)
@@ -56,23 +57,25 @@ def main(args, device):
     for _, row in df.iterrows():
 
         id_pair, embedding1name, embedding2name, x1, x2, y1, y2 = row['id_pair'], row['embedding1name'], row['embedding2name'], row['start_window1'], row['end_window1'], row['start_window2'], row['end_window2']
+        if (x2-x1 >= 12)&(y2-y1 >= 12):
+            x1_emb, x2_emb, y1_emb, y2_emb = x1//6, x2//6, y1//6, y2//6
 
-        x1_emb, x2_emb, y1_emb, y2_emb = x1//6, x2//6, y1//6, y2//6
+            embedding1_path = os.path.join(embedding_dir, embedding1name+'.npy')
+            embedding2_path = os.path.join(embedding_dir, embedding2name+'.npy')
 
-        embedding1_path = os.path.join(embedding_dir, embedding1name+'.npy')
-        embedding2_path = os.path.join(embedding_dir, embedding2name+'.npy')
+            embedding1 = np.load(embedding1_path)[x1_emb:x2_emb, :]
+            embedding2 = np.load(embedding2_path)[y1_emb:y2_emb, :]
 
-        embedding1 = np.load(embedding1_path)[x1_emb:x2_emb, :]
-        embedding2 = np.load(embedding2_path)[y1_emb:y2_emb, :]
+            rna1, rna2 =  torch.as_tensor(embedding1).unsqueeze(0), torch.as_tensor(embedding2).unsqueeze(0)
+            rna1, rna2 = torch.transpose(rna1, 1, 2), torch.transpose(rna2, 1, 2)
+            rna1, rna2 = rna1.to(device), rna2.to(device)
 
-        rna1, rna2 =  torch.as_tensor(embedding1).unsqueeze(0), torch.as_tensor(embedding2).unsqueeze(0)
-        rna1, rna2 = torch.transpose(rna1, 1, 2), torch.transpose(rna2, 1, 2)
-        rna1, rna2 = rna1.to(device), rna2.to(device)
+            outputs = model(rna1, rna2)
 
-        outputs = model(rna1, rna2)
-
-        probability += outputs.softmax(-1)[:, 1].tolist()
-        ids.append(id_pair)
+            probability += outputs.softmax(-1)[:, 1].tolist()
+            ids.append(id_pair)
+        else:
+            print(f'id: {id_pair} was not calculated because the window is too small')
 
     res = pd.DataFrame({
             'id_sample':ids,
